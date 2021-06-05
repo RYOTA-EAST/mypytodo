20b34c48e65f5ce4cc72313c96059f8ef40e2994
Ryota Higashi <ryo19870130@outlook.jp>
2021-06-05 23:57:30 +0900

    view実装

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.views.generic import(
+  ListView, DetailView, CreateView, UpdateView, DeleteView,
+)
+from django.urls import reverse_lazy
+
+from .models import Todo
 
 # Create your views here.
 
-def index(request):
-  context={}
-  return render(request, 'index.html', context)
+class IndexView(ListView):
+  
+  template_name = 'index.html'
+  model = Todo
+  context_object_name = 'todos'
+
+index = IndexView.as_view()
+
+class TodoDetailView(DetailView):
+
+  template_name = 'todo/detail.html'
+  model = Todo
+  context_object_name = 'todo'
+
+todo_detail = TodoDetailView.as_view()
+
+class TodoCreateViews(CreateView):
+
+  template_name = 'todo/create.html'
+  model = Todo
+  fields = ('title', 'body')
+  success_url = reverse_lazy('todo:index')
+
+todo_create = TodoCreateViews.as_view()
+
+class TodoUpdateViews(UpdateView):
+
+  template_name = 'todo/update.html'
+  model = Todo
+  fields = ('title', 'body')
+  success_url = reverse_lazy('todo:index')
+
+todo_update = TodoUpdateViews.as_view()
+
+class TodoDeleteViews(DeleteView):
+
+  template_name = 'todo/delete.html'
+  model = Todo
+  success_url = reverse_lazy('todo:index')
+
+todo_delete = TodoDeleteViews.as_view()
