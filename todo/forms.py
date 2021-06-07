fb930ccfa4c0bfe3b83c9b04659b3335e1ad5515
Ryota Higashi <ryo19870130@outlook.jp>
2021-06-08 01:07:40 +0900

    期限投稿・表示

--- todo/forms.py
+++ todo/forms.py
@@ -1,9 +1,11 @@
 from django import forms
-from django import forms
 from .models import Todo
 
 class TodoForm(forms.ModelForm):
 
   class Meta:
     model = Todo
-    fields = ('title', 'body')
+    fields = ('title', 'body', 'status','deadline')
+    widgets = {
+      'deadline' : forms.SelectDateWidget 
+    }
--- todo/models.py
+++ todo/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.db.models.base import Model
+from django.utils import timezone
 
 # Create your models here.
 
 class Todo(models.Model):
+  STATUS_CHOICES = [(1, '未完了'),(2, '作業中'),(3, '完了')]
 
   title = models.CharField('タイトル', max_length=55)
   body = models.TextField('詳細/内容', max_length=200)
+  status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+  deadline = models.DateTimeField('期限', default=timezone.now)
   created = models.DateTimeField('作成日時', auto_now_add=True)
   updated = models.DateTimeField('更新日時', auto_now=True)
+  
+  def publish(self):
+    self.update_at = timezone.now()
+    self.save()
   
   def __str__(self):
       return self.title
