b1f2bb9555bf5477974a4924e45aa9f114205eca
Ryota Higashi <ryo19870130@outlook.jp>
2021-10-30 23:55:32 +0900

    apiのアドレス変更

--- todo/urls.py
+++ todo/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('create', views.todo_create, name='todo_create'),
     path('update/<int:pk>', views.todo_update, name='todo_update'),
     path('delete/<int:pk>', views.todo_delete, name='todo_delete'),
-    path('api/', include(router.urls)),
+    path('api-east/', include(router.urls)),
 ]
